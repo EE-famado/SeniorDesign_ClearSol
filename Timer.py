dd59b7700061733d8d7ea306d65d26c52a4ad57c
Julian0203 <54153435+Julian0203@users.noreply.github.com>
2022-03-30 16:34:20 -0400

    Update Timer.py

--- Timer.py
+++ Timer.py
@@ -1,12 +1,15 @@
 import time
 import datetime
 
-def stop_timer(h, m, s):
+def stop_timer(input_state):
+    h = 0
+    m = 2
+    s = 0
     total_seconds = h * 3600 + m * 60 + s
     
     #While loop that checks if total_seconds reaches zero
     #If not zero, decrement total time by one second
-    while total_seconds > 0:
+    while total_seconds > 0 && input_state == False:
         #Timer represents time left on countdown
         timer = datetime.timedelta(seconds = total_seconds)
         
@@ -16,10 +19,8 @@ def stop_timer(h, m, s):
         time.sleep(1)
  
         total_seconds -= 1
-    flagon = 0
-    flagoff = 1
-    return flagon, flagoff;
-h = 0
-m = 2
-s = 0
+    
+    input_state = True
+    return  input_state;
+
 stop_timer(int(h), int(m), int(s))
